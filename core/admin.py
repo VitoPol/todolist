a1a4f81ef824aba8f54ecb7ea2ed502be08ebbc1
Viktor <viktor_polyak@mail.ru>
2023-02-09 13:50:57 +0300

    initial commit

---
+++ core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from core.models import User
+
+# Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name']
+    fields = ['username', 'email', 'first_name', 'last_name']
+    search_fields = ['email', 'first_name', 'last_name', 'username']
+    list_filter = ['is_staff', 'is_active', 'is_superuser']
+
+
+admin.site.register(User, UserAdmin)
