779c4704183c1e12c8551543d9e126eade7fa36f
Viktor <viktor_polyak@mail.ru>
2023-02-17 20:38:38 +0300

    initial commit

--- core/admin.py
+++ core/admin.py
@@ -7,9 +7,10 @@ from core.models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name']
-    fields = ['username', 'email', 'first_name', 'last_name']
+    fields = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser',
+              'date_joined', 'last_login')
+    readonly_fields = ('date_joined', 'last_login')
     search_fields = ['email', 'first_name', 'last_name', 'username']
     list_filter = ['is_staff', 'is_active', 'is_superuser']
-
 
 admin.site.register(User, UserAdmin)
