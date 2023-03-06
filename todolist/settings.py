cecf2b8dd420c1f5c9fa5b26176881ca184bdc9b
Viktor <viktor_polyak@mail.ru>
2023-03-06 16:56:03 +0300

    Some fixes

--- todolist/settings.py
+++ todolist/settings.py
@@ -154,5 +154,7 @@ APPEND_SLASH = False
 SOCIAL_AUTH_VK_OAUTH2_KEY = os.environ.get("VK_KEY", None)
 SOCIAL_AUTH_VK_OAUTH2_SECRET = os.environ.get("VK_SECRET", None)
 
+SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/categories'
 # SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
